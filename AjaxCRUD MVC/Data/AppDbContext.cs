cb96be414cf6bc2a1e72fe8a8798010d80406964
Engr. Hamid Idrees <hamididrees457@gmail.com>
2026-02-20 14:16:05 +0500

    Implement CRUD by Scaffolding

--- AjaxCRUD MVC/Data/AppDbContext.cs
+++ AjaxCRUD MVC/Data/AppDbContext.cs
@@ -9,5 +9,32 @@ namespace AjaxCRUD_MVC.Data
         {
         }
         public DbSet<Transaction> Transactions { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Transaction>().HasData(
+                new Transaction
+                {
+                    Id = 1,
+                    AccountNumber = "PK00123456789",
+                    BeneficiaryName = "Ali Khan",
+                    BankName = "HBL",
+                    SwiftCode = "HBLBPKKA",
+                    Amount = 5000m,
+                    Date = new DateTime(2026, 1, 1)
+                },
+                new Transaction
+                {
+                    Id = 2,
+                    AccountNumber = "PK00987654321",
+                    BeneficiaryName = "Sara Ahmed",
+                    BankName = "UBL",
+                    SwiftCode = "UNILPKKA",
+                    Amount = 12000m,
+                    Date = new DateTime(2026, 1, 5)
+                }
+            );
+        }
     }
 }
---
+++ AjaxCRUD MVC/Migrations/20260220091353_seedInitialData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AjaxCRUD_MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedInitialData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Transactions",
+                columns: new[] { "Id", "AccountNumber", "Amount", "BankName", "BeneficiaryName", "Date", "SwiftCode" },
+                values: new object[,]
+                {
+                    { 1, "PK00123456789", 5000m, "HBL", "Ali Khan", new DateTime(2026, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "HBLBPKKA" },
+                    { 2, "PK00987654321", 12000m, "UBL", "Sara Ahmed", new DateTime(2026, 1, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), "UNILPKKA" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Transactions",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Transactions",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
